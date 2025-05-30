fd0e48b7ec96c3940e639530ec87d06dc383cad1
Gorantla Mokshgna <gorantlamokshgna980@gmail.com>
2025-05-30 15:06:49 +0530

    removed .*/{any }

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ const __dirname = path.resolve();
 
 
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
-  app.get("/{*any}",(req,res)=>{
+  app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
   })
 
