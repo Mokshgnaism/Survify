4b11e41d274e4cad2c9871f8df0033305bba54aa
Gorantla Mokshgna <gorantlamokshgna980@gmail.com>
2025-05-30 14:58:23 +0530

    removed the impport of nnotification page from app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
-import NotificationsPage from "./pages/NotificationsPage.jsx";
+
 
 // import Ema from "./pages/OnboardingPage.jsx";
 import EmailVerificationPage from "./pages/EmailVerificationPage.jsx";
